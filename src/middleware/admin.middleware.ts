cd8bd3c756fce9a8f5cbbdf78fb9a5cb2684f68f
HalxDocs <kamsyejindu@gmail.com>
2026-01-26 13:14:02 +0100

    admin middleware update

--- src/middleware/admin.middleware.ts
+++ src/middleware/admin.middleware.ts
@@ -1,12 +1,8 @@
 // src/middleware/admin.middleware.ts
 import { Request, Response, NextFunction } from "express";
 
-export const adminOnly = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // authMiddleware MUST run first
+export const adminOnly = (req: Request, res: Response, next: NextFunction) => {
+  // authMiddleware already ran
   // @ts-ignore
   const role = req.userRole;
 
