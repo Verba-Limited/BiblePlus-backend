8cfe54e2f0fdbb63b6cc02319758ed02ae00c9fd
HalxDocs <kamsyejindu@gmail.com>
2026-02-19 09:26:41 +0100

    admin quiz done

--- src/modules/quiz/quiz.routes.ts
+++ src/modules/quiz/quiz.routes.ts
@@ -1,10 +1,26 @@
 import { Router } from "express";
 import authMiddleware from "../../middleware/auth.middleware";
 import { QuizController } from "./quiz.controller";
+import { adminOnly } from "../../middleware/admin.middleware";
+import { QuizAdminController } from "./quizAdmin.controller";
 
 const router = Router();
 
 router.get("/play", authMiddleware, QuizController.play);
 router.post("/submit", authMiddleware, QuizController.submit);
+
+router.post(
+  "/admin/add",
+  authMiddleware,
+  adminOnly,
+  QuizAdminController.add
+);
+
+router.put(
+  "/admin/deactivate/:id",
+  authMiddleware,
+  adminOnly,
+  QuizAdminController.deactivate
+)
 
 export default router;
