cc45295cf646b8ca9b0e4ae6a5227ea768aec85e
HalxDocs <kamsyejindu@gmail.com>
2026-02-25 11:56:55 +0100

    profile routes done

--- src/modules/profile/profile.routes.ts
+++ src/modules/profile/profile.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from "express";
 import authMiddleware from "../../middleware/auth.middleware";
 import { ProfileController } from "./profile.controller";
-import multer from "multer";
 
-const upload = multer({ dest: "uploads/avatars" });
 const router = Router();
 
-router.get("/", authMiddleware, ProfileController.getProfile);
+router.get("/me", authMiddleware, ProfileController.getProfile);
+
 router.put("/", authMiddleware, ProfileController.updateProfile);
-router.post(
-  "/avatar",
+
+router.put("/change-password", authMiddleware, ProfileController.changePassword);
+
+router.put(
+  "/notification-settings",
   authMiddleware,
-  upload.single("avatar"),
-  ProfileController.uploadAvatar
+  ProfileController.updateNotificationSettings
 );
+
+router.delete("/", authMiddleware, ProfileController.deleteAccount);
 
 export default router;
