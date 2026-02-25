c520973a5f2877230f71c6bd66e0ee5481af95b1
HalxDocs <kamsyejindu@gmail.com>
2026-02-25 11:49:10 +0100

    done

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ import { startDailyQuizCleanup } from "./jobs/QuizCleanup";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 const MONGO_URI = process.env.MONGO_URI as string;
 
 // -------------------------------
